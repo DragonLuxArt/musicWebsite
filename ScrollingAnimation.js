8b217cb910908a3432b8a84ff5b4befd4e6a6f8e
Bartosz Kwiczala <bartek.kwiczala@gmail.com>
2018-07-06 11:55:26 +0200

    Add Website

---
+++ ScrollingAnimation.js
@@ -0,0 +1,24 @@
+﻿$(".btn").click(function () {
+    $('html, body').animate({
+        scrollTop: $('.col-sm-3').offset().top
+    }, 2000);
+});
+
+$("#about").click(function () {
+    $('html, body').animate({
+        scrollTop: $('#AboutMe').offset().top
+    }, 2000);
+});
+
+$("#socialMedia").click(function () {
+    $('html, body').animate({
+        scrollTop: $('.col-sm-3').offset().top
+    }, 2000);
+});
+
+$("#contact").click(function () {
+    $('html, body').animate({
+        scrollTop: $('#footer').offset().top
+    },2000);
+});
+
